2c28157df42b1029bbd7216804cb5fe867bf85b2
Elhanz-Maker1 <jhonhanzortega88@gmail.com>
2025-05-26 11:17:20 -0500

    Arregle cambiar contraseña

--- app/src/main/java/com/sena/qfinder/api/ApiClient.java
+++ app/src/main/java/com/sena/qfinder/api/ApiClient.java
@@ -4,6 +4,7 @@ import java.util.concurrent.TimeUnit;
 import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import okhttp3.logging.HttpLoggingInterceptor;
 
 public class ApiClient {
     private static Retrofit retrofit = null;
@@ -13,10 +14,15 @@ public class ApiClient {
 
     public static Retrofit getClient() {
         if (retrofit == null) {
+
+            HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+            loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
             OkHttpClient okHttpClient = new OkHttpClient.Builder()
                     .connectTimeout(30, TimeUnit.SECONDS)
                     .readTimeout(30, TimeUnit.SECONDS)
                     .writeTimeout(30, TimeUnit.SECONDS)
+                    .addInterceptor(loggingInterceptor)
                     .cookieJar(cookieJar)
                     .build();
 
